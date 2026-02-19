6a067996abb89c480a305c0b7e4032696f67a5af
FLOW2203 <onlymore2024@gmail.com>
2026-02-19 08:32:57 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https' as const,
+        protocol: 'https',
         hostname: '**',
       },
     ],
@@ -19,19 +19,6 @@ const nextConfig = {
         source: '/',
         destination: '/en',
         permanent: false,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'X-DNS-Prefetch-Control', value: 'on' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
-        ],
       },
     ];
   },
